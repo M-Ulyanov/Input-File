b313b6b45ebe9d54550628c2cac97210b7933f3b
M-Ulyanov <web.ulyanov@gmail.com>
2014-05-08 15:19:39 +0400

    First Version
    Скрипт без оформления

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function(){
+
+    $('.file-button').on('click', function(){
+
+        // Определение переменных
+        var fileWrap = $(this).parent('.file-wrap'),
+        inputFile  = fileWrap.find('input[type="file"]'),
+        button = fileWrap.find('button'),
+        divTxt  = fileWrap.find('div');
+
+        // Имититация клика по inputFile
+        inputFile.click();
+
+        // Есть ли поддержка file Api
+        var fileApi = (window.File) ? true : false;
+
+        // По событию change
+        $(inputFile).on('change',function(){
+
+            var fileName;
+
+            // True и файл получен
+            if(fileApi && inputFile[0].files[0]){
+                fileName = inputFile[0].files[0].name;
+            }
+            else{
+                fileName = inputFile.val().split('\\');
+                fileName = fileName[fileName.length -1];             
+            }
+
+            // Если имя файла не получено
+            if(!fileName.length){
+                return;
+            }
+
+            // Изменяем поле с именем файла и текст на кнопки
+            divTxt.text(fileName);
+            button.text('Выбрать');
+
+        }).change();
+
+
+    });
+
+});
